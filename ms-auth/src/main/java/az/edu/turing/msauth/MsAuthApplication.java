80edc0027acf5f07b59418e106084cb99df5b5c4
akbar0660 <akbarbabayev0@gmail.com>
2025-03-20 00:26:15 +0400

    first commit

---
+++ ms-auth/src/main/java/az/edu/turing/msauth/MsAuthApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.msauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthApplication.class, args);
+    }
+
+}
