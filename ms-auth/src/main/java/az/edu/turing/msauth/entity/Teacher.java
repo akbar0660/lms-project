22af5dc05317d673f9afb9d341d4c98a4c1c5be8
akbar0660 <akbarbabayev0@gmail.com>
2025-03-23 17:51:54 +0400

    spring security attached

--- ms-auth/src/main/java/az/edu/turing/msauth/entity/Teacher.java
+++ ms-auth/src/main/java/az/edu/turing/msauth/entity/Teacher.java
@@ -2,11 +2,17 @@ package az.edu.turing.msauth.entity;
 
 import az.edu.turing.msauth.model.enums.Class;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
 @DiscriminatorValue("TEACHER")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Teacher extends UserEntity {
 
     private String specialization;
