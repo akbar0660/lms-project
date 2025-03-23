07a39e005196ac3ab0d410df7dd754ec20cfee95
akbar0660 <akbarbabayev0@gmail.com>
2025-03-23 17:51:54 +0400

    spring security attached

--- ms-auth/src/main/java/az/edu/turing/msauth/util/SuperAdminInitializer.java
+++ ms-auth/src/main/java/az/edu/turing/msauth/util/SuperAdminInitializer.java
@@ -1,15 +1,31 @@
 package az.edu.turing.msauth.util;
 
+import az.edu.turing.msauth.entity.SuperAdmin;
+import az.edu.turing.msauth.entity.UserEntity;
+import az.edu.turing.msauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@RequiredArgsConstructor
 public class SuperAdminInitializer implements CommandLineRunner {
 
-
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
-    public void run(String... args) throws Exception {
+    @Transactional
+    public void run(String... args) {
+        if (!userRepository.existsByUsername("TuringSuperAdmin")) {
+            UserEntity superAdmin = new SuperAdmin();
+            superAdmin.setUsername("TuringSuperAdmin");
+            superAdmin.setPassword(passwordEncoder.encode("TuringSuperAdmin"));
 
+            userRepository.save(superAdmin);
+        }
     }
 }
+
